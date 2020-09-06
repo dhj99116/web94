f88434ab24f6da68164dacf0abf45841320849f3
dhj99116 <2359855660@qq.com>
2020-09-06 20:59:19 +0800

    '顶部路由'

--- src/components/Sec.js
+++ src/components/Sec.js
@@ -1,73 +1,125 @@
 import React, { Component } from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
-export default class Sec extends Component {
+class Sec extends Component {
+  state = {
+    list: []
+  }
+  fun1 = (val) => {
+    let newListd = this.state.list
+    if (!newListd.includes(val)) {
+      newListd.push(val)
+    }
+    this.setState({
+      list: newListd
+    })
+  }
+  fun2=(val)=>{
+    let listd=this.state.list
+    let newData
+    if(listd.length>1){
+      newData=listd.filter(v=>{
+      return v!==val
+    })
+    this.setState({
+      list:newData
+    })
+    }
+    
+    
+    
+  }
   render() {
+    const {list} =this.state
     return (
-      <Layout style={{width:'100%',height:'100%'}}>
-    <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Layout>
-      <Sider width={200} className="site-layout-background">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
-        >
-          <SubMenu key="sub1" icon={<UserOutlined />} title="首页">
-            <Menu.Item key="1">
-            <NavLink to='/home'>评委管理</NavLink></Menu.Item>
-            <Menu.Item key="2"><NavLink to='/about'>赛事管理</NavLink></Menu.Item>
-            <Menu.Item key="3"><NavLink to='/list'>参赛管理</NavLink></Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <Content
-          className="site-layout-background"
-          style={{
-            padding: 24,
-            margin: 0,
-            minHeight: 280,
-          }}
-        >
-         {this.props.children}
-        </Content>
+      <Layout style={{ width: '100%', height: '100%' }}>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="首页">
+                <Menu.Item key="1">
+                  <NavLink to='/home' onClick={() => this.fun1('/home')}>评委管理</NavLink>
+                 
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <NavLink to='/about' onClick={() => this.fun1('/about')}>赛事管理</NavLink>
+                  
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <NavLink to='/list' onClick={() => this.fun1('/list')}>参赛管理</NavLink>
+              
+                </Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="6">option6</Menu.Item>
+                <Menu.Item key="7">option7</Menu.Item>
+                <Menu.Item key="8">option8</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                <Menu.Item key="9">option9</Menu.Item>
+                <Menu.Item key="10">option10</Menu.Item>
+                <Menu.Item key="11">option11</Menu.Item>
+                <Menu.Item key="12">option12</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              <div className='topp'>
+                {
+                  list.map((v,i)=>{
+                    let a=v.split('')
+                    a.shift()
+                    a=a.join('')
+                    return (
+                      <div key={v}>
+                      {a}
+                        <span onClick={() => this.fun2(v)}>X</span>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+              {this.props.children}
+            </Content>
+          </Layout>
+        </Layout>
       </Layout>
-    </Layout>
-  </Layout>
-     
-        
-      
+
+
+
     )
   }
 }
+export default withRouter(Sec)
