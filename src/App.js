27d013e3bea3155d2effaa0b7c4c76a8d23f2004
dhj99116 <2359855660@qq.com>
2020-09-06 16:30:07 +0800

    'layout布局'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='box'>
         <BrowserRouter>
           <Switch>
             <Route path='/login' component={Login} />
